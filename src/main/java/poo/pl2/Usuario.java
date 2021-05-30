621ac0b97d7c9f4d15f4504f046e103aa998cc90
ferib <ferib@Wally>
2021-05-30 13:42:32 +0200

    He creado la clase Usuario

---
+++ src/main/java/poo/pl2/Usuario.java
@@ -0,0 +1,29 @@
+package poo.javaflix;
+
+public class Usuario {
+    
+    private final String correo;
+    private String clave;
+    
+    public Usuario (String correo, String clave){
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return "Correo: "+correo+". Clave: "+clave+'.';
+    }
+}
