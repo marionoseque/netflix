2125895c72103a728081f3022a734434a91ad012
ferib <ferib@Wally>
2021-05-30 13:57:01 +0200

    He creado la clase Cliente

---
+++ src/main/java/poo/pl2/Cliente.java
@@ -0,0 +1,36 @@
+package poo.PL2;
+
+public class Cliente extends Usuario{
+   
+    private String dni;
+    private String nombre;
+
+    public Cliente(String dni, String nombre, String correo, String clave) {
+        super(correo, clave);
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+" DNI: "+dni+". Nombre: "+nombre;
+    }
+
+    
+}
