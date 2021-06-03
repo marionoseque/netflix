7bc01a40be22cf57ee172b7c88c8d65f7731faad
eduardogarciahuerta <eduardogarciahuerta@localhost>
2021-06-03 13:24:16 +0200

    He añadido la clase "Pelicula"

---
+++ src/main/java/poo/pl2/Pelicula.java
@@ -0,0 +1,32 @@
+package poo.pl2;
+
+import java.util.ArrayList;
+
+public class Pelicula extends Contenido {
+    private int duracion;
+    private String director;
+
+    public Pelicula(int duracion, String director, String titulo, String sinopsis, String genero, int año, ArrayList<String> actores) {
+        super(titulo, sinopsis, genero, año, actores);
+        this.duracion = duracion;
+        this.director = director;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+", duración:"+duracion+", director:"+director; 
+    }
+}
