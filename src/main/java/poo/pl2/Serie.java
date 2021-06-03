35d1d4cf0d53f8fd390633c8a0ff415a5748ae83
eduardogarciahuerta <eduardogarciahuerta@localhost>
2021-06-03 13:24:44 +0200

    He añadido la clase "Serie "

---
+++ src/main/java/poo/pl2/Serie.java
@@ -0,0 +1,36 @@
+package poo.pl2;
+
+import java.util.ArrayList;
+
+public class Serie extends Contenido {
+    private int temporadas;
+    private Capitulo capitulo;
+
+    public Serie(int temporadas, Capitulo capitulo, String titulo, String sinopsis, String genero, int año, ArrayList<String> actores) {
+        super(titulo, sinopsis, genero, año, actores);
+        this.temporadas = temporadas;
+        this.capitulo = capitulo;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+    public Capitulo getCapitulo() {
+        return capitulo;
+    }
+    public void setCapitulo(Capitulo capitulo) {
+        this.capitulo = capitulo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"nº de temporadas:"+temporadas+", capitulo:"+capitulo; 
+    }
+
+    
+    
+    
+}
